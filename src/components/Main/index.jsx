b2538eed9cea122dc3b74d2c109d0a585c7e9414
Madiba Hudson-Quansah <mhquansah@gmail.com>
2023-04-12 12:18:23 +0000

    (init): Bootstrapping

---
+++ src/components/Main/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Main = () => {
+    return (
+        <>
+            <h1>Hello World</h1>
+            <p>WIP</p>
+        </>
+    );
+};
+
+export default Main;
