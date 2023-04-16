55e4c3024ac57d8e3d6de890c994c3fd8593624d
Madiba Hudson-Quansah <mhquansah@gmail.com>
2023-04-16 03:16:08 +0000

    (feat): Improved header component

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,11 +1,9 @@
 import React from "react";
 
+import Header from "../Header";
 const Main = () => {
     return (
-        <>
-            <h1>Hello World</h1>
-            <p>WIP</p>
-        </>
+            <Header title="Verifyx" />
     );
 };
 
