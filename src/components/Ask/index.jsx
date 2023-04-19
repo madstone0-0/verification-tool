65e398c29780d607e0c53d6931a0695217b6da2c
Madiba Hudson-Quansah <mhquansah@gmail.com>
2023-04-19 20:34:28 +0000

    (dev): Added response component and improved persona

--- src/components/Ask/index.jsx
+++ src/components/Ask/index.jsx
@@ -1,18 +1,27 @@
 import React from "react";
 import Response from "../Response";
 import Prompt from "../Prompt";
+import { usePrompts } from "../hooks";
+import { Box } from "@mui/material";
 
-const Ask = ({ onPromptChange, onPromptSubmit, prompt, output, loading }) => {
+const Ask = ({ onPromptChange, onPromptSubmit }) => {
+    const { output, loading, error, prompt } = usePrompts();
     return (
-        <>
+        <Box textAlign="center">
             <Prompt
                 loading={loading}
                 prompt={prompt}
                 onPromptChange={onPromptChange}
                 onPromptSubmit={onPromptSubmit}
             />
-            {output ? <Response output={output} /> : <></>}
-        </>
+            {error ? (
+                <div>Error communicating with server please try again</div>
+            ) : output ? (
+                <Response output={output} />
+            ) : (
+                <></>
+            )}
+        </Box>
     );
 };
 
