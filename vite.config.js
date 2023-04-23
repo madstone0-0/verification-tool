ad98a9cd658cb09bc9935c2983ec6fae55211431
Madiba Hudson-Quansah <mhquansah@gmail.com>
2023-04-23 16:39:45 +0000

    (build): Made build script

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => {
     return {
+        build: {},
         plugins: [react()],
     };
 });
